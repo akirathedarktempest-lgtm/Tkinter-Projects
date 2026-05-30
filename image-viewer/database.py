a3c231d73d0b9396b060ef442fdb1c8bc1b656da
purple(?) <akirathedarktempest@gmail.com>
2026-05-30 16:00:21 +0530

    Implement image path saving GUI with SQLite
    Added a GUI for saving image file paths to a SQLite database. The application allows users to input image paths and validates the file extensions before saving.

--- image-viewer/database.py
+++ image-viewer/database.py
@@ -1,14 +1,40 @@
 import sqlite3
+from tkinter import *
+from pathlib import Path
 
 connect=sqlite3.connect("image_address.db")
 cursor=connect.cursor()
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS data(
                address text)""")
+root=Tk()
+root.title("Image Add!")
+myLab=Label(root,text="...")
+myLab.pack()
 
-ads=input()
+def imagePath():
+    e=str(entry.get())
+    if "\\" in e:
+        e=e.replace("\\","/")
+    path=Path(e)
+    if path:
+        if ".jpg" in e or ".png" in e or ".jpeg" in e or ".gif" in e or ".webp" in e:
+            cursor.execute("INSERT INTO data VALUES (?)",[e])
+            connect.commit()
+            entry.delete(0,END)
+            myLab.config(text="Saved!")
+        else:
+            myLab.config(text="No such image file is there! The image extension can jpg, jpeg, png, webp, gif")
+    else:
+        myLab.config(text="There's file like that :(")
 
-cursor.execute("INSERT INTO data VALUES (?)",[ads])
-connect.commit()
-connect.close()
-#we will insert the addresses of images available in your device, and if the images are in same folder where the files are, then just write the name of the image, like, image.png or any extension the image file has
+entry=Entry(root)
+entry.pack()
+
+button=Button(root,text="Save!",command=imagePath)
+button.pack()
+
+root.mainloop()
+#this is now an application to save the image adresses, a small app will open, and you will put the location, if it has similar extensions like jpg etc, and if it's present then it will save it to the database
+#keep in mind, if you put something like, C:\Images\image don't forget to add the extension, if you find and see there's no extension, look at properties and the extension it mentions, that should be added like
+#C:\Images\image.png this is an example and you can get the extension by looking at the properties of the file
