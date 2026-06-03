19565685cac6660738ff093a63e5cf4cead87c3e
purple(?) <akirathedarktempest@gmail.com>
2026-06-03 17:10:19 +0530

    Enhance image viewer functionality and UI
    Refactor image navigation logic and improve status display.

--- image-viewer/image-viewer.py
+++ image-viewer/image-viewer.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from PIL import ImageTk,Image
-import sqlite3#we will use sqlite3 to stare the address of the images
+import sqlite3
 
 root=Tk()
 root.title("Image Viewer!")
@@ -11,40 +11,57 @@ cursor=connect.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS data(
                address text)""")
 cursor.execute("SELECT * FROM data")
-images=cursor.fetchall()#we will use fetchall, all the image addresses
+images=cursor.fetchall()
 
-y=0#this will work as indexing, like the first image would be 0 and so on
-myImg=ImageTk.PhotoImage(Image.open(images[y][0]))#in sqlite, we are doing double indexing, why? y is the indexing of the image and will give (image_address.png,), why "," because a tuple having one statement is treated like that
+status=Label(root,text=f"Image 1 of {len(images)}")#this will be like the number of image, like if you view image 2, it will show at below "Image 2 of 5" if total are 5
+
+y=0
+myImg=ImageTk.PhotoImage(Image.open(images[y][0]))
 myLab=Label(root,image=myImg)
 myLab.grid(row=0,column=1,columnspan=5)
 
-def leftside():#this will make a button and keep on changing images here
+def leftside():
     global y
-    global myImg#use myImg, same name because it will be a trouble, the image wouldn't show if it's not a global variable
-    y-=1#this moves the image a little bit behind
-    if y<-(len(images)):#now why this? because indexing can have error, if you keep on moving back, 0 will appear by default, then, -1,-2 and so on, but the first one will come one day and if you try to move back once again, error
+    global myImg
+    global status
+    y-=1
+    if y<-(len(images)):
         y=len(images)-1
         myImg=ImageTk.PhotoImage(Image.open(images[y][0]))
         myLab.config(image=myImg)
+        rowid=row_id(images[y][0])
+        status.config(text=f"Image {rowid} of {len(images)}")
     else:
         myImg=ImageTk.PhotoImage(Image.open(images[y][0]))
         myLab.config(image=myImg)
+        rowid=row_id(images[y][0])
+        status.config(text=f"Image {rowid} of {len(images)}")
     
-def rightside():#same as the leftside, but it was making it minus behind, but this will show of ahead, like, 0 index, 1, then 2
+def rightside():
     global y
     global myImg
+    global status
     y+=1
     if y==len(images):
         y=0
-        myImg=ImageTk.PhotoImage(Image.open(images[y][0]))
-        myLab.config(image=myImg)
+        myImg=ImageTk.PhotoImage(Image.open(images[y][0]))#you all may ask, I am using the function there but not here, why, because that will start showing in negative and wrong index, but it wouldn't happen in positive index
+        myLab.config(image=myImg)#if you click on image 5 and move on, it will turn 1 again, because y will turn 0 and we are doing y+1 here
+        status.config(text=f"Image {y+1} of {len(images)}")#in case of negative indexing, it works like this, Image 1, 0+1=y+1, if you click back, it will show 0, and then more negative, so just to keep it simple and as much I could use my logic, I have done and used rowid
     else:
         myImg=ImageTk.PhotoImage(Image.open(images[y][0]))
         myLab.config(image=myImg)
+        status.config(text=f"Image {y+1} of {len(images)}")
 
-left_button=Button(root,text="<-",command=leftside)#buttons to move left or right
+def row_id(img_adr):
+    cursor.execute(f"SELECT rowid FROM data WHERE address=\"{img_adr}\"")
+    number=cursor.fetchone()
+    return number[0]
+
+left_button=Button(root,text="<-",command=leftside)
 right_button=Button(root,text="->",command=rightside)
-left_button.grid(row=1,column=0)#packing is important
-right_button.grid(row=1,column=6)#this is now for better UX...i am not good at ui/ux, but i am trying to be better
+left_button.grid(row=1,column=0)
+right_button.grid(row=1,column=6)
+
+status.grid()
 
 root.mainloop()
